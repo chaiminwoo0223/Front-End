19a7addf79fa92a2b227c9d90991ed2411937a8c
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-18 15:31:31 +0900

    프론트엔드 개발

--- NODE1/chap06/person5.js
+++ NODE1/chap06/person5.js
@@ -6,5 +6,5 @@ let person1 = createPerson();
 let person2 = createPerson();
 person1.name = "임꺽정";
 person2.age = 20;
-console.log(person1)
-console.log(person2)
+console.log(person1);
+console.log(person2);
