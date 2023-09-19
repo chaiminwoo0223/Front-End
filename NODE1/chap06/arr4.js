ae0a6fef9628e02accec3c0d0d3c404c5e77572a
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-19 10:06:09 +0900

    프론트엔드 개발

---
+++ NODE1/chap06/arr4.js
@@ -0,0 +1,11 @@
+let persons = [
+    { name: "홍길동", age: 16 },
+    { name: "임꺽정", age: 18 }
+];
+console.log(persons);
+
+persons = [
+    { name: "전우치", age: 20 },
+    { name: "이몽룡", age: 20 }
+];
+console.log(persons);
