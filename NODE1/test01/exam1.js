7ff83a364da3a26e3923a68b32e209f911e50ce1
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-28 17:36:16 +0900

    Delete NODE1/test01 directory

--- NODE1/test01/exam1.js
+++
@@ -1,6 +0,0 @@
-// print 함수 구현
-a = true, b = "hello world", c = undefined
-
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
--- NODE1/test01/exam6.js
+++
@@ -1,9 +0,0 @@
-// remove(a, index) 함수 구현
-function remove(a, index) {
-    a.splice(index, 1);
-}
-
-let a = [1, 2, 3, 4, 5];
-remove(a, 1);
-remove(a, 2);
-console.log(a);
