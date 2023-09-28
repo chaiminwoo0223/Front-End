481f6bfbd80da0b7cc5e1cef4131c166d7e48c80
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-28 21:26:17 +0900

    Delete NODE1/test02 directory

--- NODE1/test02/exam02.js
+++
@@ -1,7 +0,0 @@
-function printSum(a, b) { // 이 함수를 구현하라. 
-    console.log(a.toString() + b.toString());
-} 
-
-printSum(1, 2); 
-printSum(3, 4); 
-printSum(12, 34);
--- NODE1/test02/exam08.js
+++
@@ -1,6 +0,0 @@
-function countSpace(s) {
-    return s.match(/\s/g).length;
-  }
-  
-  console.log(countSpace("aa bb ccc"));
-  console.log(countSpace("a b cc ddd  e"));
