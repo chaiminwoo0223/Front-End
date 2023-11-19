13d4bce47dcd6efc959c29b535806a76fb8b7a75
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-11-19 17:29:21 +0900

    프론트엔드 개발

--- NODE1/test03/exam3.js
+++
@@ -1,6 +0,0 @@
-function testCallback(f, a, b) {
-    console.log(f(a, b));
-}
-  
-testCallback((a, b) => a + b, 3, 4);
-testCallback((a, b) => a * b, 3, 4);
--- NODE1/test03/exam4.js
+++
@@ -1,6 +0,0 @@
-function createObj(value) {
-    return { value: value };
-}
-  
-console.log(createObj(3));
-console.log(createObj("hello"));  
